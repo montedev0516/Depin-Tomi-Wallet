b2b636bdf1e7c70789830564612395539c9a0c84
montedev0516 <montecristodev0516@gmail.com>
2024-08-11 19:57:16 -0500

    change to server app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-"use client"
-
 import { useRouter } from "next/navigation";
-import Pic1 from "../../public/favicon.ico"
-import Pic2 from "../assets/images/image1.png"
-import Image from "next/image";
-import { useEffect } from "react";
+// import Pic1 from "../../public/favicon.ico";
+// import Pic2 from "../assets/images/image1.png"
+// import Image from "next/image";
+// import { useEffect } from "react";
 import StartLogo from "@/components/ui/StartLogo";
 
 export default function Page() {
@@ -13,19 +11,19 @@ export default function Page() {
     const nextPage = () => {
         router.push('/ConnectWallet')
     }
-    const checkRAM = () => {
-        console.log("-----> ", navigator)
-    }
+    // const checkRAM = () => {
+    //     console.log("-----> ", navigator)
+    // }
 
-    useEffect(() => {
-        checkRAM()
-    }, [])
+    // useEffect(() => {
+    //     checkRAM()
+    // }, [])
     return (
         <>
             <div className="flex flex-col items-center md:px-[12vw] px-[8vw] md:py-[12vh] py-[8vh] h-screen text-white" onClick={nextPage}>
                 <div className="flex flex-row gap-2 items-center">
                     <div>
-                        <Image src={Pic1} alt="Icon" className="md:w-16 md:h-16 w-10 h-10"></Image>
+                        {/* <Image src={Pic1} alt="Icon" className="md:w-16 md:h-16 w-10 h-10"></Image> */}
                     </div>
                     <div>
                         <p className="md:text-6xl text-4xl font-bold">Node</p>
