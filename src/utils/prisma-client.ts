ab9c2619f34a9903b329b0573c678d146f81412c
montedev0516 <montecristodev0516@gmail.com>
2024-08-12 22:03:22 -0500

    deleted all master branch's files

--- src/utils/prisma-client.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
-
-export const prismaClient = globalForPrisma.prisma || new PrismaClient()
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prismaClient
-
-export default prismaClient
--- src/utils/thirdweb-client.ts
+++
@@ -1,7 +0,0 @@
-import { createThirdwebClient } from 'thirdweb'
-
-const CLIENT_ID = process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID;
-
-export const thirdwebClient = createThirdwebClient({
-    clientId: CLIENT_ID!
-})
