2169135d73044a137393583b63d133c8228a9177
montedev0516 <montecristodev0516@gmail.com>
2024-08-13 14:47:34 +1000

    set File Icon

--- main/background.ts
+++ main/background.ts
@@ -25,7 +25,7 @@ if (isProd) {
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
-    icon:"./resources/faviicon.ico"
+    // icon:"./faviicon.ico"
   })
 
   if (isProd) {
